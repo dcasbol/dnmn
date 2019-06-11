312a5ce4c8865a12a62ef35a211508af3216ac20
David <davidcastillobolado@hotmail.com>
2019-06-11 13:23:52 +0100

    more readable condition in VQAFindDataset filtering

--- vqatorch.py
+++ vqatorch.py
@@ -201,7 +201,7 @@ class VQAFindDataset(VQADataset):
 				ans = set()
 				for a in q['answers']:
 					ans.add(a)
-				if len(ans) == 1 and len(ans.intersection(neg_set)) > 0:
+				if len(ans) == 1 and ans.pop() in neg_set:
 					continue
 			self._imap.append(i)
 
