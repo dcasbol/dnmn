1ab77ea53d0b536ad99a4a123f32605a9489d29a
David <davidcastillobolado@hotmail.com>
2019-06-18 14:40:04 +0100

    compute normalizers in float32 and for python3

--- scripts/compute_normalizers.py
+++ scripts/compute_normalizers.py
@@ -1,27 +1,39 @@
+import os
+import argparse
 import numpy as np
 from glob import iglob
 
 IMAGE_PAT = "/home/david/DataSets/vqa/Images/train2014/conv/*.jpg.npz"
 
-def compute_normalizers():
-	mean = np.zeros((512,))
-	mmt2 = np.zeros((512,))
+def compute_normalizers(fn_pattern):
+	mean = np.zeros((512,), dtype=np.float32)
+	mmt2 = np.zeros((512,), dtype=np.float32)
 	count = 0
-	for fn in iglob(IMAGE_PAT):
+	for fn in iglob(fn_pattern):
 		with np.load(fn) as zdata:
 			assert len(zdata.keys()) == 1
-			image_data = zdata[zdata.keys()[0]]
+			image_data = list(zdata.values())[0]
 			sq_image_data = np.square(image_data)
 			mean += np.sum(image_data, axis=(0,1))
 			mmt2 += np.sum(sq_image_data, axis=(0,1))
 			count += image_data.shape[0] * image_data.shape[1]
-	mean /= float(count)
-	mmt2 /= float(count)
+	assert count > 0, "glob didn't match any file"
+	mean /= count
+	mmt2 /= count
 	var = mmt2 - np.square(mean)
 	std = np.sqrt(var)
 
 	return mean, std
 
 if __name__ == '__main__':
-	mean, std = compute_normalizers()
+
+	parser = argparse.ArgumentParser(description='Compute image normalizers')
+	parser.add_argument('datasetpath', type=str, default='~/DataSets/vqa',
+		help='Path to VQA dataset.')
+	args = parser.parse_args()
+
+	fn_pattern = os.path.join(args.datasetpath, 'Images/train2014/conv/*.jpg.npz')
+	fn_pattern = os.path.expanduser(fn_pattern)
+
+	mean, std = compute_normalizers(fn_pattern)
 	np.savez_compressed('normalizers.npz', mean=mean, std=std)
