1335f4a38656337405924f56ced7b3d11a2591da
David Castillo <5521007+dcasbol@users.noreply.github.com>
2019-07-24 10:56:36 +0100

    move sps2 to their own dir, to avoid conflicts with data and links

--- misc/constants.py
+++ misc/constants.py
@@ -1,5 +1,5 @@
 QUESTION_FILE = "data/vqa/Questions/OpenEnded_mscoco_%s_questions.json"
-MULTI_PARSE_FILE = "data/vqa/Questions/%s.sps2"
+MULTI_PARSE_FILE = "multiparse/%s.sps2"
 ANN_FILE = "data/vqa/Annotations/mscoco_%s_annotations.json"
 IMAGE_FILE = "data/vqa/Images/%s/conv/COCO_%s_%012d.jpg.npz"
 RAW_IMAGE_FILE = "data/vqa/Images/%s/raw/COCO_%s_%012d.jpg"
