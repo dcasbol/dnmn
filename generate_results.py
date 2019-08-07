6b3fe3f0971dbe4ba93a8e9c0059c33e25bd5456
David <5521007+dcasbol@users.noreply.github.com>
2019-08-07 18:20:35 +0100

    option to use relu in generate_results

--- generate_results.py
+++ generate_results.py
@@ -24,6 +24,7 @@ if __name__ == '__main__':
 	parser.add_argument('--measure')
 	parser.add_argument('--nmn')
 	parser.add_argument('--output', default='results.json')
+	parser.add_argument('--find-mode', choices=['pre', 'post', 'softmax'], default=None)
 	args = parser.parse_args()
 
 	modules = ['encoder', 'find', 'describe', 'measure']
@@ -38,7 +39,7 @@ if __name__ == '__main__':
 		shuffle    = False
 	)
 
-	nmn = NMN()
+	nmn = NMN(find_mode=args.find_mode)
 	if args.nmn is not None:
 		nmn.load(args.nmn)
 	else:
--- model.py
+++ model.py
@@ -6,9 +6,9 @@ from misc.indices import ANSWER_INDEX
 
 class NMN(torch.nn.Module):
 
-	def __init__(self, dropout=False):
+	def __init__(self, dropout=False, find_mode=None):
 		super(NMN, self).__init__()
-		self._find = Find(competition=None)
+		self._find = Find(competition=find_mode)
 		self._describe = Describe(normalize_attention=False)
 		self._measure = Measure(dropout=dropout)
 		self._encoder = QuestionEncoder(dropout=dropout)
