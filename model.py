76d97830e072c29286def8d8fd7f4c87c102ed78
David <5521007+dcasbol@users.noreply.github.com>
2019-08-16 12:15:45 +0100

    remove profiling code

--- model.py
+++ model.py
@@ -1,10 +1,24 @@
 import torch
 import torch.nn.functional as F
-from misc.util import cudalize, DEVICE, Chronometer
+from misc.util import cudalize, DEVICE
 from modules import Find, Describe, Measure, QuestionEncoder
 from misc.indices import ANSWER_INDEX
 
+""" LAST PROFILING MADE
+
+00:18:28/00:05:58  24% - 2.6149481296539308
+yesno 16.357530117034912
+maps 55.134193897247314
+empty 0.02860236167907715
+inst 8.076877117156982
+others 16.136756896972656
+qenc 11.334144115447998
+do 0.6323862075805664
+"""
+
 class NMN(torch.nn.Module):
+
+	NAME = 'nmn'
 
 	def __init__(self, dropout=False):
 		super(NMN, self).__init__()
@@ -21,43 +35,31 @@ class NMN(torch.nn.Module):
 			False : lambda x: x,
 			True  : lambda x: F.dropout2d(x, p=0.5, training=self.training)
 		}[dropout]
-		self._chr = { k: Chronometer() for k in ['do', 'inst', 'maps', 'empty', 'yesno', 'others', 'qenc'] }
 
 	def forward(self, features, question, length, yesno, root_inst, find_inst):
 
 		# this is the equivalent to dropout over 1x1 kernel
 		# Drop here to drop the same features for all modules
-		self._chr['do'].start()
 		features = self._dropout2d(self._dropout(features))
 		features_list = features.unsqueeze(1).unbind(0)
-		self._chr['do'].stop()
 
-		self._chr['inst'].start()
 		find_inst = [ torch.tensor(inst, dtype=torch.long, device=DEVICE) for inst in find_inst ]
-		self._chr['inst'].stop()
 
-		self._chr['maps'].start()
 		maps = list()
 		for f, inst in zip(features_list, find_inst):
 			f = f.expand(len(inst), -1, -1, -1)
 			m = self._find[inst](f).prod(0, keepdim=True)
 			maps.append(m)
 		maps = torch.cat(maps)
-		self._chr['maps'].stop()
 
-		self._chr['empty'].start()
 		root_pred = torch.empty(yesno.size(0), len(ANSWER_INDEX), device=DEVICE)
-		self._chr['empty'].stop()
 
-		self._chr['yesno'].start()
 		yesno_maps = maps[yesno]
 		if yesno_maps.size(0) > 0:
 			yesno_inst = root_inst[yesno]
 			yesno_ans = self._measure[yesno_inst](yesno_maps)
 			root_pred[yesno] = yesno_ans
-		self._chr['yesno'].stop()
 
-		self._chr['others'].start()
 		other = ~yesno
 		other_maps = maps[other]
 		if other_maps.size(0) > 0:
@@ -65,12 +67,9 @@ class NMN(torch.nn.Module):
 			other_fts  = features[other]
 			other_ans  = self._describe[other_inst](other_maps, other_fts)
 			root_pred[other] = other_ans
-		self._chr['others'].stop()
 
-		self._chr['qenc'].start()
 		root_pred = root_pred.softmax(1)
 		enc_pred = self._encoder(question, length).softmax(1)
-		self._chr['qenc'].stop()
 
 		return (root_pred*enc_pred + 1e-30).sqrt()
 
@@ -91,7 +90,3 @@ class NMN(torch.nn.Module):
 		assert module_name in {'find', 'describe', 'measure', 'encoder'}
 		module = getattr(self, '_'+module_name)
 		torch.save(module.state_dict(), filename)
-
-	def show_times(self):
-		for k, c in self._chr.items():
-			print(c, c.read())
