270e204879c200e82a24ced3b246822e1a2ddc14
David Castillo <5521007+dcasbol@users.noreply.github.com>
2019-10-16 10:53:38 +0100

    plot accuracies in relative scale

--- plots/gauge_correlation_plot.py
+++ plots/gauge_correlation_plot.py
@@ -9,6 +9,13 @@ def get_args():
 	parser.add_argument('input_log')
 	return parser.parse_args()
 
+def norm_factors(values):
+	min_val = min(values)
+	max_val = max(values)
+	return min_val, 1/(max_val-min_val+1e-10)
+
+normalize = lambda vs, d, s: [ (v-d)*s for v in vs ]
+
 def main(args):
 
 	with open(args.input_log) as fd:
@@ -16,19 +23,24 @@ def main(args):
 
 	plt.figure()
 	plt.title('Influence of Find module')
-	plt.xlabel('Find module at epoch')
-	plt.ylabel('Accuracy (%)')
+	plt.xlabel('Find stage (epoch)')
+	plt.ylabel('Relative Accuracy')
 
 	epochs = [ e+1 for e in data['epoch'] ]
-	mean   = [ m*100 for m in data['nmn_top_1_mean'] ]
-	std    = [ s*100 for s in data['nmn_top_1_std'] ]
+	mean   = data['nmn_top_1_mean']
+	std    = data['nmn_top_1_std']
+
+	displ, scale = norm_factors(mean)
+	mean   = normalize(mean, displ, scale)
+	std    = normalize(std, 0, scale)
 
 	y1 = [ m-2*d for m, d in zip(mean, std) ]
 	y2 = [ m+2*d for m, d in zip(mean, std) ]
 	plt.fill_between(epochs, y1, y2, alpha=0.3)
 	plt.plot(epochs, mean, label='End2end NMN $\pm 2 \sigma$')
 
-	top_1 = [ a*100 for a in data['find_top_1'] ]
+	top_1 = data['find_top_1']
+	top_1 = normalize(top_1, *norm_factors(top_1))
 	plt.plot(epochs, top_1, label='Gauge validation')
 
 	plt.legend()
