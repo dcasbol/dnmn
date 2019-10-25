8cd4c3abccb826e0c9ebfe0df1c466bbd5063765
David <5521007+dcasbol@users.noreply.github.com>
2019-10-25 13:01:57 +0100

    deprecate activation selection in Find

--- modules.py
+++ modules.py
@@ -57,24 +57,18 @@ class Find(InstanceModule):
 
 	NAME = 'find'
 
-	def __init__(self, activation='relu', **kwargs):
+	def __init__(self, **kwargs):
 		super(Find, self).__init__(dropout=0, **kwargs)
 		self._conv = nn.Conv2d(IMG_DEPTH, len(FIND_INDEX), 1, bias=False)
 		self._conv.weight.data.fill_(0.01)
-		self._act_fn = dict(
-			elu   = lambda x: F.elu(x) + 1.0,
-			srelu = lambda x: F.softsign(x)*0.5 + 0.5 + x.relu(),
-			relu  = lambda x: x.relu()
-		)[activation]
 
 	def forward(self, features):
 		c = self._get_instance()
 		B,D,H,W = features.size()
 		kernel = self._conv.weight[c]
 		group_feats = features.contiguous().view(1,B*D,H,W)
-		hmap = F.conv2d(group_feats, kernel, groups=B)
-		hmap = self._act_fn(hmap)
-		return hmap.view(B,1,H,W)
+		hmap = F.conv2d(group_feats, kernel, groups=B).relu().view(B,1,H,W)
+		return hmap
 
 	def loss(self, pred, target):
 		raise NotImplementedError
