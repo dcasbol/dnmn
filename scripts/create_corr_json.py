d712c5e1055f9de7fe13e2444f940faf66b78e6f
David <5521007+dcasbol@users.noreply.github.com>
2019-11-06 11:46:34 +0000

    extend for considering variance

--- scripts/create_corr_json.py
+++ scripts/create_corr_json.py
@@ -4,22 +4,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 from glob import glob
 
-VALUE_KEY  = 'top_1'
 MIN_VALUE  = 0.01
 N_VALUES   = 30
 N_SLOTS    = 10
 
-if __name__ == '__main__':
+def get_args():
+	descr = """Create JSON with selected Find modules for correlation plot"""
+	parser = argparse.ArgumentParser(description=descr)
+	parser.add_argument('--input-pattern', default='find-rnd/find-rnd*.json')
+	parser.add_argument('--max-variance', type=float, default=0.1)
+	return parser.parse_args()
 
-	glob_list = glob('find-rnd/find-rnd*.json')
+def main():
+
+	args = get_args()
+
+	glob_list  = glob(args.input_pattern)
+	var_list   = list()
 	value_list = list()
-	fn_list = list()
+	fn_list    = list()
+	skipped_acc = skipped_var = 0
 	for fn in glob_list:
 		with open(fn) as fd:
-			v = json.load(fd)[VALUE_KEY]
-		if v > MIN_VALUE:
-			value_list.append(v)
-			fn_list.append(fn)
+			d = json.load(fd)
+		if d['top_1'] < MIN_VALUE:
+			skipped_acc += 1
+			continue
+		if d.get('var',0) > args.max_variance:
+			skipped_var += 1
+			continue
+
+		value_list.append(d['top_1'])
+		fn_list.append(fn)
+		if 'var' in d:
+			var_list.append(d['var'])
+
+	for n, reason in [(skipped_acc, 'accuracy'), (skipped_var, 'variance')]:
+		if n == 0: continue
+		print('{} values skipped due to {} constraints')
 
 	N_PER_SLOT = int(N_VALUES/N_SLOTS)
 	min_v = min(value_list)
@@ -41,15 +63,24 @@ if __name__ == '__main__':
 				sel_filenames.append(fn)
 		N_PER_SLOT += 1
 
-	print('{} points to {}'.format(len(value_list), len(sel_values)))
+	print('{} selected from {}'.format(len(sel_values), len(value_list)))
 
 	plt.figure()
 	plt.hist(sel_values)
 	plt.show()
 
 	plt.figure()
-	plt.scatter(value_list, [0.5]*len(value_list), c='grey', alpha=0.5)
-	plt.scatter(sel_values, [1]*len(sel_values), alpha=0.5)
+	if len(var_list) == 0:
+		plt.scatter(value_list, [0.5]*len(value_list), c='grey', alpha=0.5)
+		plt.scatter(sel_values, [1]*len(sel_values), alpha=0.5)
+	else:
+		excl_indices = [ i for i in range(len(value_list)) if i not in sel_indices ]
+		excl_values = [ value_list[i] for i in excl_indices ]
+		excl_vars   = [ var_list[i] for i in excl_indices ]
+		plt.scatter(excl_values, excl_vars, c='grey', alpha=0.5)
+
+		sel_vars = [ var_list[i] for i in sel_indices ]
+		plt.scatter(sel_values, sel_vars, alpha=0.5)
 	plt.show()
 
 	if input('Write to JSON file? ').lower() in {'y','yes'}:
@@ -60,6 +91,10 @@ if __name__ == '__main__':
 			gauge_accs = sel_values,
 			nmn_accs   = [-1]*len(sel_values)
 		)
+		if len(var_list) > 0:
+			data['gauge_vars'] = sel_vars
 		with open('gauge_corr_data.json', 'w') as fd:
 			json.dump(data, fd)
 
+if __name__ == '__main__':
+	main()
