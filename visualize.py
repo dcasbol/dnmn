caaf217d1093e13eeb19fba356da026ec2195766
David <5521007+dcasbol@users.noreply.github.com>
2020-02-11 12:30:32 +0000

    use MapVisualizer in visualize script. --modular flag

--- visualize.py
+++ visualize.py
@@ -1,14 +1,12 @@
 import argparse
 import torch
 import time
-import numpy as np
-import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from vqa import VQAFindDataset
 from modules import Find
 from misc.indices import FIND_INDEX
 from misc.constants import *
-from PIL import Image
+from misc.visualization import MapVisualizer
 
 parser = argparse.ArgumentParser(description='Visualize Find Module')
 parser.add_argument('ptfile', type=str,
@@ -18,6 +16,7 @@ parser.add_argument('condition', type=str, nargs='?', default="",
 parser.add_argument('--wait', type=float, default=1.)
 parser.add_argument('--shuffle', action='store_true')
 parser.add_argument('--dataset', choices=['train2014', 'val2014'], default='val2014')
+parser.add_argument('--modular', action='store_true')
 args = parser.parse_args()
 
 cond = -1
@@ -27,13 +26,11 @@ if len(args.condition) > 0:
 findset = VQAFindDataset(metadata=True)
 loader = DataLoader(findset, batch_size=1, shuffle=args.shuffle)
 
-find = Find()
+find = Find(modular=args.modular)
 find.load_state_dict(torch.load(args.ptfile, map_location='cpu'))
 find.eval()
 
-plt.figure()
-plt.ion()
-plt.show()
+vis = MapVisualizer(1)
 
 for features, instance, instance_str, input_set, input_id in loader:
 
@@ -41,22 +38,6 @@ for features, instance, instance_str, input_set, input_id in loader:
 		continue
 
 	hmap = find[instance](features)
-
-	plt.clf()
-	plt.suptitle(instance_str[0])
-
-	ax = plt.subplot(1,2,1)
-	img = hmap.detach()[0,0].cpu().numpy()
-	im = plt.imshow(img, cmap='hot')
-	plt.colorbar(im, orientation='horizontal', pad=0.05)
-	plt.axis('off')
-
-	plt.subplot(1,2,2)
-	fn = RAW_IMAGE_FILE % (input_set[0], input_set[0], input_id[0])
-	img = np.array(Image.open(fn).resize((300,300)))
-	plt.imshow(img)
-	plt.axis('off')
-
-	plt.draw()
-	plt.pause(0.001)
+	vis.update(hmap, instance_str, input_set, input_id)
+	
 	time.sleep(args.wait)
