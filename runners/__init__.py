eda445aab3a9924f2f16b11bd76a92d148f94f0b
David Castillo <5521007+dcasbol@users.noreply.github.com>
2020-02-17 11:43:08 +0000

    remove uncached from init

--- runners/__init__.py
+++ runners/__init__.py
@@ -3,11 +3,9 @@ from .encoder_runner import EncoderRunner
 from .find_runner import FindRunner
 from .describe_runner import DescribeRunner
 from .measure_runner import MeasureRunner
-from .uncached_runners import DescribeRunnerUncached, MeasureRunnerUncached
 
 del nmn_runner
 del encoder_runner
 del find_runner
 del describe_runner
 del measure_runner
-del uncached_runners
