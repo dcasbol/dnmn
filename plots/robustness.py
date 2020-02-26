81e72d1699a65bfbd864d3aa690e79f04f13a203
David <5521007+dcasbol@users.noreply.github.com>
2020-02-26 12:32:21 +0000

    robustness plot

---
+++ plots/robustness.py
@@ -0,0 +1,39 @@
+import argparse
+import re
+import glob
+import os
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+MODULE_NAMES = ['find','measure','describe','encoder']
+
+def get_args():
+	parser = argparse.ArgumentParser(description='Modular robustness plot')
+	parser.add_argument('hpo_dir')
+	parser.add_argument('--nmn-hpo')
+	return parser.parse_args()
+
+def _get_acc(fn):
+	m = re.search('^[a-z]+-(\d{3}\.\d).+_log\.json$', fn)
+	return float(m.group(1))
+
+def _get_accs_dict(path, name=None):
+	pat = path if name is None else os.path.join(path, name)
+	pat = os.path.join(pat, '*_log.json')
+	name = 'nmn' if name is None else name
+	return [ { 'name':name, 'accuracy':_get_acc(fn) } for fn in glob.glob(pat) ]
+
+def main(args):
+
+	data = list()
+	for name in MODULE_NAMES:
+		data.extend(_get_accs_dict(args.hpo_dir, name))
+	data.extend(_get_accs_dict(args.nmn_hpo))
+
+	data = pd.DataFrame(data)
+	sns.swarmplot(x='name', y='accuracy', data=data)
+	plt.show()
+
+if __name__ == '__main__':
+	main(get_args())
