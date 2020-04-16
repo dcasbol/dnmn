79dbc54f9e7c196c60850b610d151200033909bb
David <5521007+dcasbol@users.noreply.github.com>
2020-04-16 12:12:31 +0100

    allow 2-layer describe from runner

--- runners/describe_runner.py
+++ runners/describe_runner.py
@@ -2,11 +2,21 @@ from .base import Runner
 from model import Describe
 from loaders import DescribeLoader
 from misc.util import cudalize
+from misc.constants import *
 
 class DescribeRunner(Runner):
 
+	def __init__(self, hidden_size=None, hidden_dropout=0, **kwargs):
+		self._hidden_size    = hidden_size
+		self._hidden_dropout = hidden_dropout
+		super(DescribeRunner, self).__init__(**kwargs)
+
 	def _get_model(self):
-		return Describe(dropout=self._dropout)
+		return Describe(
+			dropout        = self._dropout,
+			hidden_size    = self._hidden_size,
+			hidden_dropout = self._hidden_dropout
+		)
 
 	def _get_loader(self, **kwargs):
 		return DescribeLoader(prior=self._modular, **kwargs)
