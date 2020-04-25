4f67ca143253ec4eef2675526e84891d033bf163
David <5521007+dcasbol@users.noreply.github.com>
2020-04-25 13:42:02 +0100

    new criteria in show_corr. Fix potential bug

--- show_corr_data.py
+++ show_corr_data.py
@@ -8,6 +8,8 @@ from scipy.stats import pearsonr
 def get_args():
 	parser = argparse.ArgumentParser(description='Show gauge-nmn correlation plot')
 	parser.add_argument('--data-json', default='gauge_corr_data.json')
+	parser.add_argument('--criterion',
+		choice=['accs','rel_accs','loss'], default='loss')
 	return parser.parse_args()
 
 def main(args):
@@ -15,22 +17,27 @@ def main(args):
 	with open(args.data_json) as fd:
 		d = json.load(fd)
 
-	nmn_accs   = [ a for a in d['nmn_accs'] if a>0 ]
-	gauge_accs = d['gauge_accs'][:len(nmn_accs)]
-	gauge_accs = [ a*100 for a in gauge_accs ]
+	gauge_k = 'gauge_' + args.criterion
+	nmn_k   = 'nmn_'   + args.criterion
+
+	values = [ (nv, gv) for nv, gv in zip(d[nmn_k], d[gauge_k]) if nv>0 ]
+	nmn_values, gauge_values = zip(*values)
+	if 'accs' in args.criterion:
+		gauge_values = [ v*100 for v in gauge_values ]
 	print('Showing {} points'.format(len(gauge_accs)))
 
-	r, p = pearsonr(gauge_accs, nmn_accs)
+	r, p = pearsonr(gauge_values, nmn_values)
 	print('coeff: ', r)
 	print('p-value: ', p)
 
+	k = 'accuracy (%)' if 'accs' in args.criterion else 'loss'
+	if 'rel' in args.criterion:
+		k = 'relative ' + k
+
 	plt.figure()
-	vars = d['gauge_vars'][:len(gauge_accs)]
-	mv = max(vars)
-	lv = min(vars)
-	gk = 'Gauge accuracy (%)'
-	nk = 'NMN accuracy (%)'
-	df = pd.DataFrame(zip(gauge_accs, nmn_accs),
+	gk = 'Gauge ' + k
+	nk = 'NMN ' + k
+	df = pd.DataFrame(zip(gauge_values, nmn_values),
 		columns=[gk,nk])
 	sns.regplot(x=gk, y=nk, data=df)
 	plt.show()
