289aee93e2088bb8ebb11c462777670ffac8f9d2
David <5521007+dcasbol@users.noreply.github.com>
2020-05-30 13:55:54 +0100

    exclude cache & w/o title

--- plots/times.py
+++ plots/times.py
@@ -9,7 +9,6 @@ def get_args():
 	descr = "Plot aggregated time data from the hyperparameter optimization"
 	parser = argparse.ArgumentParser(description=descr)
 	parser.add_argument('hpo_dir', default='hyperopt')
-	parser.add_argument('--cache-logs', nargs=2)
 	parser.add_argument('--raw-times', action='store_true')
 	return parser.parse_args()
 
@@ -30,19 +29,11 @@ def collect_time_data(args):
 			for k in ['time', 'raw_time']:
 				data[k][name] += log[k][-1]
 
-	for fn in args.cache_logs:
-		with open(fn) as fd:
-			log = json.load(fd)
-		for k in ['time', 'raw_time']:
-			data[k]['cache'] += log[k]
-
 	return data
 
 def plot_data(args, data):
 
-	suf = '(raw)' if args.raw_times else '(forward-backward)'
 	plt.figure()
-	plt.title('Training times '+suf)
 	plt.ylabel('Hours')
 
 	palette = {
@@ -50,8 +41,7 @@ def plot_data(args, data):
 		'measure'    : 'orange',
 		'describe'   : 'green',
 		'encoder'    : 'red',
-		'end-to-end' : 'purple',
-		'cache'      : 'black'
+		'end-to-end' : 'purple'
 	}
 
 	bottom = 0
@@ -60,7 +50,7 @@ def plot_data(args, data):
 
 	time_key = 'raw_time' if args.raw_times else 'time'
 
-	NAMES = ['encoder', 'find', 'cache', 'measure', 'describe']
+	NAMES = ['encoder', 'find', 'measure', 'describe']
 	for name in NAMES:
 		t = data[time_key][name]/3600
 		p = plt.bar(0, t, width, bottom=bottom, color=palette[name])
@@ -71,7 +61,7 @@ def plot_data(args, data):
 	plt.bar(1, t, width, color=palette['end-to-end'])
 
 	plt.xticks((0, 1), ('modular', 'end-to-end'))
-	plt.legend(plots, NAMES)
+	plt.legend(plots, [ name.capitalize() for name in NAMES ])
 
 	plt.show()
 
